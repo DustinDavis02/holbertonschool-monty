e2e896b331c4e77cd3d0315d40ee8186b71aea4f
Dustin Davis <davisdustin328@gmail.com>
2022-11-22 21:20:45 -0800

    fixing opcode selector

--- get_OP.c
+++ get_OP.c
@@ -2,7 +2,6 @@
 
 /**
 * get_opcode - selects the right function for the correct opcode.
-*
 * @str which opcode
 * Return: a function pointer.
 */
@@ -15,16 +14,13 @@ void (*get_opcode(char *str))(stack_t **stack, unsigned int line_number);
 		      {"pint", pint},
 		      {"pop", pop},
 		      {"swap", swap},
-		      {"add", add},
-		      {"nop", nop},
 		      {NULL, NULL},
 	};
+	unsigned int i = 0;
 
-	int i = 0;
-
-	while (opcode_func[i].opcode != NULL && strcmp(opcode_func[i].opcode, str) != 0)
+	while (opcode_func[i].opcode && strcmp(str, opcode_func[i].opcode) != 0)
 		i++;
-
-	return (opcode_func[i].f);
-	}
-	
+	if (opcode_fun[i].opcode == NULL)
+		return (NULL);
+	return (opcode_fun[i].f);
+}
